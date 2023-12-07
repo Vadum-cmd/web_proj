19635d608d67f06f0077f47d167a12bb545a85a1
Vadum-cmd <vadum.treskot@gmail.com>
2023-12-07 12:43:10 +0200

    trying things out

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000; // PORT
 
-app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('hehe');
+})
+
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+})
+
+app.get('/api.cources/:year/:month', (req, res) => {
+    res.send(req.params);
+})
 
 
+app.listen(port, () => console.log(`Listening on port ${port}...`));
